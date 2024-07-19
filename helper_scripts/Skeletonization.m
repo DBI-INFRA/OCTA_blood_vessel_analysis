43a18c7c618cd5505622ea758a26b703398b7e7a
Anony <40377608+Paddy-Xu@users.noreply.github.com>
2024-07-19 11:01:44 +0200

    update file path

--- helper_scripts/Skeletonization.m
+++ helper_scripts/Skeletonization.m
@@ -34,32 +34,11 @@ image_frangi = frangi_2Dfilter(image_median, frangi_opts);
 % Apply fuzzy thresholding to binarize & segment the image
 binarized_img = fuzzy_thresholding(image_frangi, 2, 3) - 1; % nth = 2 clusters
 
-% 
-% options = struct('FrangiScaleRange', [1 6], 'FrangiScaleRatio', 2, ...
-%                  'FrangiBetaOne', 0.5, 'FrangiBetaTwo', 15, ...
-%                  'BlackWhite', true); % Adjust these parameters as needed
-% 
-% [J, Scale, Direction] = FrangiFilter2D(image);
-% 
-% figure;
-% subplot(1, 2, 1);
-% imshow(image_median./255);
-% title('Original Image');
-% subplot(1, 2, 2);
-% imshow(J, []);
-% title('Frangi Filtered Image');
-% level = graythresh(image_frangi); % Compute Otsu's threshold level
-% binarized_img_otsu = imbinarize(image_frangi, level); % Binarize the image using the computed threshold
 
 
 % Skeletonize the binary image
 skeletonized_img = bwmorph(binarized_img, 'skel', Inf);
 
-% image_frangi = (image_frangi - min(image_frangi(:))) / (max(image_frangi(:)) - min(image_frangi(:)));
-
-% imwrite(uint8(image_median), 'image_median.png');
-% imwrite(uint8(image_frangi* 255), 'image_frangi.png');
-% imwrite(uint8(binarized_img * 255), 'binarized_img.png');
 
 if VISUALIZE
     % Display the processed image at the different steps
@@ -70,13 +49,6 @@ if VISUALIZE
     subplot(1,4,3); imshow(binarized_img); title('3. Fuzzy thresholding');
     subplot(1,4,4); imshow(skeletonized_img); title('4. Skeletonization');
 
-    % subplot(2,4,1); imshow(image./255); title('1. Original image');
-    % subplot(2,4,2); imshow(image_median./255); title('2. Median filtering');
-    % subplot(2,4,3); imshow(image_frangi); title('3. Frangi filtering');
-    % subplot(2,4,4); imshow(binarized_img); title('4. Fuzzy thresholding');
-    % subplot(2,4,5); imshow(skeletonized_img); title('5. Skeletonization');
-
-    % subplot(2,4,6); imshow(binarized_img_otsu); title('6. binarized_img_otsu');
 
     F1.WindowState = 'maximized';
 
