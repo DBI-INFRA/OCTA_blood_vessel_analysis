a0d56c934dd53b1961d3a46412ba073955ff58a6
mertesdorfj <49643546+mertesdorfj@users.noreply.github.com>
2024-07-26 14:53:52 +0200

    Clarified parameter information for OCTA_2

--- OCTA_2_CLD_SPD_estimation.m
+++ OCTA_2_CLD_SPD_estimation.m
@@ -48,25 +48,28 @@ frangi_opts.sigmastepsize = 1;
 frangi_opts.correctionconst1 = 0.8;
 frangi_opts.correctionconst2 = 15; 
 
-% The method to apply for thresholding, i.e. dividing the image into foreground (vessels) & background
-% The selectable options are: "local_adaptive_thresholding", "fuzzy_thresholding", "otsu_thresholding"
-thresholding.method = "local_adaptive_thresholding";
-
-% The sensitivity of the local adaptive thresholding algorithm (higher values will
-% pick up more of the vessels & smaller vessels, but potentially also more noise)
-% Note: This parameter is only relevant if you selected "local_adaptive_thresholding"
-% thresholding.sensitivity = 0.3;
-
-% note that this parameter is only for script #2
-thresholding.sensitivity = 0;
-% opening will remove some islands after binarization, based on the area
-% size (in pixels), adjust if necessary
+% Morphological opening will remove some small islands after segmentation, based on 
+% the area size (in pixels). This helps to remove noise. Adjust if necessary
 thresholding.opening_size = 75;
 
 % ================= Print parameters & parse input ========================
 % Print the selected parameters & image path
 fprintf("\nIMAGE FOLDER PATH: \n  <%s>\n", result_path);
 fprintf('\nSELECTED PARAMETERS:\n  Median filter size: %d \n  Frangi filter: sigma range: [%d, %d], sigma stepsize: %d, correctionconst1: %.2f, correctionconst 2: %d\n\n', median_filter_size, frangi_opts.sigmarange(1), frangi_opts.sigmarange(2), frangi_opts.sigmastepsize, frangi_opts.correctionconst1, frangi_opts.correctionconst2);
+
+% IMPORTANT NOTE: The thresholding method + sensitivity is now fixed for OCTA_2, since this
+%                 variant worked best for accurate CLD & SPD detection.
+% The method to apply for thresholding, i.e. dividing the image into foreground (vessels) & background
+% The selectable options are: "local_adaptive_thresholding", "fuzzy_thresholding", "otsu_thresholding"
+thresholding.method = "local_adaptive_thresholding";
+
+% The sensitivity of the local adaptive thresholding algorithm (higher values will
+% pick up more & smaller vessels, but potentially also more noise).
+% IMPORTANT: The thresholding sensitivity should be set to 0 for OCTA_2 to improve  
+% the accuracy of the CLD-detection. Note that it is however not necessarily recommended  
+% to use sensitivity=0 for OCTA_3 - the ideal value should be selected based on visual 
+% inspection of the thresholding results for different sensitivity values.
+thresholding.sensitivity = 0;
 
 % Add new user parameters to parameters table
 params_path = fullfile(result_path, 'InputParameters.mat');
