663003d80a6aa2eb0986fc22249498d438940f28
jeskowagner <jesko.wagner@sund.ku.dk>
2025-12-08 10:28:06 +0100

    export binary tifs as uncompressed labels\nThis enables napari to view the exported tifs correctly

--- OCTA_3_MorphQuant.m
+++ OCTA_3_MorphQuant.m
@@ -167,9 +167,9 @@ for ff = 1:length(filelist)
         writetable(vessel_measurements, [save_path '_perBranchMeasurements.csv'])
 
         % Write Integer Output
-        imwrite(binary_img, [save_path '_vessels.tif'])
-        imwrite(skeleton, [save_path '_skeleton.tif'])
-        imwrite(uint16(labeled_skeleton), [save_path '_labeledSkeleton.tif'])
+        imwrite(~binary_img, [save_path '_vessels.tif'], compression="none")
+        imwrite(~skeleton, [save_path '_skeleton.tif'], compression="none")
+        imwrite(uint16(labeled_skeleton), [save_path '_labeledSkeleton.tif'], compression="none")
 
         % Plot mapped measurements
         turbo_on_black = [0, 0, 0; turbo(255)];
